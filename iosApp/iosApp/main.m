408ade26a8234ed305ad95e57d8d30e32dc217a2
klejdi <kskender@student.42heilbronn.de>
2026-03-22 11:45:49 +0100

    ready

---
+++ iosApp/iosApp/main.m
@@ -0,0 +1,23 @@
+#import <UIKit/UIKit.h>
+#import <shared/shared.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = [SharedMain_iosKt MainViewController];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
--- shared/src/iosMain/kotlin/com/pawtracker/main.ios.kt
+++ shared/src/iosMain/kotlin/com/pawtracker/main.ios.kt
@@ -1,7 +1,10 @@
+@file:OptIn(kotlinx.cinterop.ExperimentalForeignApi::class)
+
 package com.pawtracker
 
 import androidx.compose.ui.window.ComposeUIViewController
+import platform.UIKit.UIViewController
 
 actual fun getPlatformName(): String = "iOS"
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController(): UIViewController = ComposeUIViewController { App() }
